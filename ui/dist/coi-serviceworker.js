880a71f2c7b9cf6a81309a95883bde9f84719d62
ttezer <ttezer@gmail.com>
2026-06-02 14:20:06 +0300

    fix(coi-sw): fetch hatasi undefined yerine exception firlatsin

--- ui/dist/coi-serviceworker.js
+++ ui/dist/coi-serviceworker.js
@@ -54,7 +54,7 @@ if (typeof window === 'undefined') {
                         headers: newHeaders,
                     });
                 })
-                .catch((e) => console.error(e))
+                .catch((e) => { console.error(e); throw e; })
         );
     });
 
