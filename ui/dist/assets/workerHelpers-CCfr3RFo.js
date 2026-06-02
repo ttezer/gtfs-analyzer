ecd48c99bc57f16445669c52e4f05b038c7e7a8c
ttezer <ttezer@gmail.com>
2026-06-02 18:46:54 +0300

    build: STM_012 LA Metro fix icin wasm + dist yeniden derlendi

--- ui/dist/assets/workerHelpers-CCfr3RFo.js
+++ ui/dist/assets/workerHelpers-CCfr3RFo.js
@@ -8,7 +8,7 @@ let __tla = (async ()=>{
         });
     }
     o(self, "wasm_bindgen_worker_init").then(async ({ init: r, receiver: t })=>{
-        const e = await import("./gtfs_wasm-BAun7j3C.js");
+        const e = await import("./gtfs_wasm-AQKMZNrI.js");
         await e.default(r), postMessage({
             type: "wasm_bindgen_worker_ready"
         }), e.wbg_rayon_start_worker(t);
